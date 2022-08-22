c746f3547df1512a37feb5b2da1d56c056ca6c97
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-22 10:46:42 -0300

    Update

--- crud_r/src/App.jsx
+++ crud_r/src/App.jsx
@@ -1,11 +1,59 @@
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ModalComp from "./components/ModalComp";
 
-
+/* App */
 const App = () => {
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [data, setData] = useState([]);
+  const [dataEdit, setDataEdit] = useState([]);
+
   return (
-    <>
-     <h1>Olá, Mundo... Sapup3 na Área!</h1> 
-    </>
+    <Flex
+      h="100vh"
+      align="center"
+      justify="center"
+      fontSize="20px"
+      fontFamily="poppins"
+    >
+      <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
+        <Button colorScheme="blue" onClick={() => [setDataEdit({}), onOpen()]}>
+          Novo Cadastro
+        </Button>
+
+        <Box overflow="auto" height="100%">
+          <Table mt="6">
+            <Thead>
+              <Tr>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                  Nome
+                </Th>
+                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                  E-Mail
+                </Th>
+                <Th p={0}></Th>
+                <Th p={0}></Th>
+              </Tr>
+            </Thead>
+          </Table>          
+        </Box>
+      </Box>
+     
+    </Flex>
   )
 }
 
