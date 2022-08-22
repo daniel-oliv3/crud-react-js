b317466efafbe6d6e3852816a019e25627d7c2e1
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-08-22 09:51:14 -0300

    Update

--- crud_r/src/components/ModalComp.jsx
+++ crud_r/src/components/ModalComp.jsx
@@ -1,10 +1,49 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    FormControl, 
+    Input,
+    Box,
 
+} from "@chakra-ui/react"
 
-const ModalComp = () => {
+import { useState } from "React";
+
+/* ModalComp */
+const ModalComp = ({ data, setData, dataEdit, isOpen, onClose }) => {
+
+    const [name, setName] = useState(dataEdit.name || "");
+    const [email, setEmail] = useState(dataEdit.email || "");
 
     return (
       <>
-       <h3>Components ModalC</h3> 
+    
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Cadastro de Clientes</ModalHeader>
+                <ModalCloseButton />
+                <ModalBady>
+                    <FormControl display="flex" flexDir="column" gap={4}>
+                        <Box>
+                            <FormLabel>Nome</FormLabel>
+                            <Input 
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </Box>
+                    </FormControl>
+                </ModalBady>
+            </ModalContent>
+        </Modal>
+
       </>
     )
   }
